9fa00d46cca15aae232b3b7b70e0675758380a9e
Marcos Romero Lamas <marromlam@gmail.com>
2021-12-07 17:12:00 +0100

    move src to module

--- lib99ocl/version.py
+++ lib99ocl/version.py
@@ -2,7 +2,7 @@
 Auto-generated module holding the version of the lib99ocl package
 '''
 
-VERSION = "0.0.0dev1"
-VERSION_INFO = [0, 0, 0, 'dev', 1]
+VERSION = "0.0.0dev2"
+VERSION_INFO = [0, 0, 0, 'dev', 2]
 
 __all__ = ['VERSION', 'VERSION_INFO']
